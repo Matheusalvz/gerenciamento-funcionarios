c05f24397a09f161c3617d1a5cb6b06edeb4da52
Matheus <teu5991@gmail.com>
2022-09-21 17:26:52 -0300

    Criação do módulo Funcionarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmpresasModule } from './empresas/empresas.module';
 import { SetoresModule } from './setores/setores.module';
+import { FuncionariosModule } from './funcionarios/funcionarios.module';
 
 @Module({
-  imports: [EmpresasModule, SetoresModule],
+  imports: [EmpresasModule, SetoresModule, FuncionariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
