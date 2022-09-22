cdfd7e78e5d9ef04b6bc5d197110756e9f7cd9c0
Matheus <teu5991@gmail.com>
2022-09-22 17:51:24 -0300

    Ajustes na estrutura do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmpresasModule } from './empresas/empresas.module';
 import { SetoresModule } from './setores/setores.module';
 import { FuncionariosModule } from './funcionarios/funcionarios.module';
+import { PrismaService } from './database/PrismaService';
 
 @Module({
   imports: [EmpresasModule, SetoresModule, FuncionariosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  // controllers: [AppController],
+  // providers: [PrismaService],
 })
 export class AppModule {}
